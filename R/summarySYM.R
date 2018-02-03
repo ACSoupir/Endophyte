5f20bc44ec886611666eca7e2850c4312b3a0361
ACSoupir <34483643+ACSoupir@users.noreply.github.com>
2018-02-02 18:16:38 -0600

    Add files via upload

--- R/summarySYM.R
+++ R/summarySYM.R
@@ -15,7 +15,7 @@
 #' summarySYM(plant_merged, measurevar="Root_Length", change="Control", groupvars=c("Isolate","Nutrient"), na.rm=TRUE)
 #' summary_table <- summarySYM(plant_merged, measurevar="Root_Length", groupvars="Isolate", na.rm=TRUE)
 
-summarySYM <- function(data=NULL, measurevar, change="NULL", groupvars=NULL, na.rm=FALSE,
+summarySYM <- function(data=NULL, measurevar, change=NULL, groupvars=NULL, na.rm=FALSE,
                                 conf.interval=temp, .drop=TRUE) {
 
   # New version of length which can handle NA's: if na.rm==T, don't count them
@@ -26,7 +26,7 @@ summarySYM <- function(data=NULL, measurevar, change="NULL", groupvars=NULL, na.
 
   # This does the summary. For each group's data frame, return a vector with
   # N, mean, median, and sd
-  datac <- ddply(data, groupvars, .drop=.drop,
+  data2 <- ddply(data, groupvars, .drop=.drop,
                  .fun = function(xx, col) {
                    c(N    = length2(xx[[col]], na.rm=na.rm),
                      mean = mean   (xx[[col]], na.rm=na.rm),
@@ -37,30 +37,33 @@ summarySYM <- function(data=NULL, measurevar, change="NULL", groupvars=NULL, na.
                  measurevar
   )
 
-  datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
+  data2$se <- data2$sd / sqrt(data2$N)  # Calculate standard error of the mean
 
   # Confidence interval multiplier for standard error
   # Calculate t-statistic for confidence interval:
   # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
-  ciMult <- qt(conf.interval/2 + .5, datac$N-1)
-  datac$ci <- datac$se * ciMult
+  ciMult <- qt(conf.interval/2 + .5, data2$N-1)
+  data2$ci <- data2$se * ciMult
 
   #sets the variable in the table to what you want the
   i=1
-  if(change != "NULL"){
-    while(change != datac[i,1]){
+  if(!is.null(change)){
+    while(change != data2[i,1]){
       i=i+1
     }
 
   #calculates the change for each variable set before
-  datac$change <- ((datac[,"mean"] - datac[i,"mean"]) / datac[i,"mean"])
+  data2$change <- ((data2[,"mean"] - data2[i,"mean"]) / data2[i,"mean"])
 
   }
 
   # Rename the "mean" column
-  datac <- rename(datac, c("mean" = measurevar))
+  data2 <- rename(data2, c("mean" = measurevar))
+
+  #run dunnTest for nonparametric comparisons
+  data3 <-
 
   #returns the new table
   # use "<- summarySYM()" to set global table
-  return(datac)
+  return(data2)
 }
