1dd6131312a5833cc19433d6fe757ced67e0d365
ACSoupir <34483643+ACSoupir@users.noreply.github.com>
2018-05-11 16:57:36 -0500

    Add files via upload

--- R/addDunn.R
+++ R/addDunn.R
@@ -37,21 +37,33 @@ addDunn <- function(data, summaryData, dunnComp, padj = NULL){
   dunn <- dunnTest(data[[i]] ~ data[[j]], data, method = padj, two.sided = FALSE)[["res"]]
 
   df <- rename(data.frame(str_split_fixed(dunn$Comparison, " - ", 2)),
-               c("X1"=colnames(summaryData)[1],"X2"="X2"))
+               c("X1"="X1","X2"="X2"))
 
   df2 <- data.frame(cbind(df,dunn))
-  df2 = df2[df2$X2 == dunnComp,]
+  df3 <- df2
+  df2 = df2[df2$X1 == dunnComp,]
 
-  if(padj=="none"){
-    df2 <- df2[,-c(2:4,6)]
-    colnames(df2)[2] <- "Dunn.P.unadj"
-  }else{
-    df2 <- df2[,-c(2:4)]
-    colnames(df2)[2] <- "Dunn.P.unadj"
-    df2 <- rename(df2, c("P.adj" = padj))
+  if(is.na(df2[1,1]) == TRUE){
+    df3 = df3[df3$X2 == dunnComp,]
+    df3 = df3[,-c(2,3,4)]
+    df3 <- rename(df3, c("X1" = x))
+    t=1
+  } else{
+    df3 = df3[df3$X1 == dunnComp,]
+    df3 = df3[,-c(1,3,4)]
+    df3 <- rename(df3, c("X2" = x))
+    t=0
   }
 
-  df3 <- merge(summaryData, df2, by.x=colnames(summaryData)[1], by.y=colnames(df2)[1], all.x=TRUE)
+  if(padj=="none"){
+    df3 <- df3[,-c(3)]
+    colnames(df3)[2] <- "Dunn.P.unadj"
+  }else{
+    colnames(df3)[2] <- "Dunn.P.unadj"
+    df3 <- rename(df3, c("P.adj" = padj))
+  }
 
-  return(df3)
+  df4 <- merge(summaryData, df3, by.x=colnames(summaryData)[1], by.y=colnames(df3)[1], all.x=TRUE)
+
+  return(df4)
 }
